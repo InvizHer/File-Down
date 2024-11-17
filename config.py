1b80c7d72aec14f7df078cb5a14cfd442b6d0fb0
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-11-17 08:24:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("5457862323:AAFVMwvqyeVjMoYCLmzvfjzu2pGXSXvjRwA")
-MONGODB_URI = os.getenv("mongodb+srv://codexun:TeamCodexun07@codexun.egmx5.mongodb.net/?retryWrites=true&w=majority")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+MONGODB_URI = os.getenv("MONGODB_URI")
 DATABASE_NAME = "file_storage_bot"
 COLLECTION_NAME = "files"
-BASE_URL = os.getenv("BASE_URL", "https://inz-file-down.onrender.com")  # Update this with your Render URL
+BASE_URL = os.getenv("BASE_URL", "https://your-app-name.onrender.com")  # Update this with your Render URL
 MAX_FILE_SIZE = 50 * 1024 * 1024  # 50MB max file size
